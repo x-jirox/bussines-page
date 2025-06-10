e10b6efe2c6c25100e8c7a7624874e191a4956f1
x-jirox <correalenin045@gmail.com>
2025-06-10 15:40:59 -0500

    design: Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Navbar.css";
 import { Link } from "react-scroll";
 import Logo from "../../assets/logo/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -8,7 +9,7 @@ function Navbar() {
   const menuIcon = <FontAwesomeIcon icon={faBars} />;
 
   return (
-    <header id="site-header">
+    <header id="site_header">
       <div className="container">
         <nav className="navbar" id="Navbar">
           <div className="navbar_brand">
@@ -25,18 +26,23 @@ function Navbar() {
                 </Link>
               </li>
               <li>
-                <Link activeClass="active" to="home" spy={true} smooth={true}>
-                  Home
+                <Link activeClass="active" to="about" spy={true} smooth={true}>
+                  About Us
                 </Link>
               </li>
               <li>
-                <Link activeClass="active" to="home" spy={true} smooth={true}>
-                  Home
+                <Link activeClass="active" to="services" spy={true} smooth={true}>
+                  Services
                 </Link>
               </li>
               <li>
-                <Link activeClass="active" to="home" spy={true} smooth={true}>
-                  Home
+                <Link activeClass="active" to="blog" spy={true} smooth={true}>
+                  Blog
+                </Link>
+              </li>
+                            <li>
+                <Link activeClass="active" to="contact" spy={true} smooth={true}>
+                  Contact Us
                 </Link>
               </li>
             </ul>
