1b8e019bf3789743ac9a5270ecbe45589a2f1c02
x-jirox <correalenin045@gmail.com>
2025-06-13 21:49:53 -0500

    components/Services: Add Services component with styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import About from './components/About/About';
+import Services from './components/Services/Services';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Services />
     </div>
   );
 }
