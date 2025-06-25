a41c8b538604317ff33fe252a6ccfb3755db4e9e
x-jirox <correalenin045@gmail.com>
2025-06-24 21:39:43 -0500

    proyect: Add Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react';
-// import './Footer.css';
+import './Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
 
